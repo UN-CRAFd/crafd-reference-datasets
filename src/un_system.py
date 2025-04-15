42fd216064273e3bd8faf76967dc7312021df862
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-04-15 18:30:49 -0400

    feat: add un-system data

---
+++ src/un_system.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+data_folder = Path("data")
+
+url = "https://www.un.org/en/about-us/un-system"
+
+# add headers to avoid 403 ERROR
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+response = requests.get(url, headers=headers)
+
+with open(data_folder / "input" / "un-system.html", "w", encoding="utf-8") as file:
+    file.write(response.text)
+
+########################################################################################
+
+df = pd.read_csv(data_folder / "output" / "un-system.csv")
