2c1d3691520c359e38bd30346354b12d88ce7ed8
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-06-27 16:48:45 -0400

    move to uv

--- setup.sh
+++ setup.sh
@@ -1,46 +1,6 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
-## Setup
+# curl -LsSf https://astral.sh/uv/install.sh | sh
 
-brew update && brew upgrade pyenv
-
-## Check current up-to-date version
-# https://www.python.org/downloads/
-
-if [ ! -f .python-version ]; then
-    echo ".python-version not found!"
-    exit 1
-fi
-
-# cat .python-version
-# pyenv versions
-
-# Skip with N if already installed
-pyenv install "$(cat .python-version)"
-
-# Set local pyenv version for this repo
-pyenv local "$(cat .python-version)"
-
-# check the currently active Python version
-pyenv version
-pyenv which python
-
-# remove existing virtual environment if it exists
-if [ -d ".venv" ]; then
-    echo "Removing existing virtual environment..."
-    rm -rf .venv
-fi
-
-# create virtual environment
-python -m venv .venv
-
-# load venv
-source .venv/bin/activate
-
-# check virtual environment
-which python
-python --version
-
-# install packages
-pip install -r requirements.txt
+uv sync
+cp .env.example .env
